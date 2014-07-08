99cb346c1b0e4e9da4babb9af9654e5c934f58af
shellfly <shell0fly@gmail.com>
2014-07-08 16:49:35 +0800

    update dist info

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-vote',
-    version='1.0.2',
+    version='1.0.3',
     packages=['vote'],
     include_package_data=True,
     license='BSD License',  
