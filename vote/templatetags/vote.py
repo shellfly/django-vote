0041a478aca40d0f82a9206edde53e5cb53a7e28
Alejandro Villanueva <admin@ialex.org>
2016-03-01 10:06:12 -0500

    Adding anonymous user when user is not logged in

--- vote/templatetags/vote.py
+++ vote/templatetags/vote.py
@@ -1,7 +1,11 @@
 from django import template
+from django.contrib.auth.models import AnonymousUser
 
 register = template.Library()
 
+
 @register.simple_tag
-def vote_exists(model, user):
+def vote_exists(model, user=AnonymousUser()):
+    if user.is_anonymous():
+        return False
     return model.votes.exists(user)
