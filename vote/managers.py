d80274c9975102f4e5a07f1ded61a6c307a09f35
Philippe Luickx <philippe.luickx@gmail.com>
2017-05-31 18:22:34 +0300

    Provide the user vote in manager

--- vote/managers.py
+++ vote/managers.py
@@ -130,6 +130,13 @@ class _VotableManager(models.Manager):
             return False
 
     @instance_required
+    def user_vote(self, user_id):
+        return self.through.objects.filter(
+            user_id=user_id,
+            content_object=self.instance,
+        )
+        
+    @instance_required
     def exists(self, user_id, action=UP):
         return self.through.objects.filter(
             user_id=user_id,
