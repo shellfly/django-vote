e4bc10c53bc3d57f326a294fdda94d6cea0890dc
DoctorJohn <github@ehwald.info>
2018-05-04 19:05:05 +0200

    Fix vote_exists template tag for django 2.0

--- vote/templatetags/vote.py
+++ vote/templatetags/vote.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django import template
 from django.contrib.auth.models import AnonymousUser
+from django import get_version
 
 from vote.models import UP
 
@@ -10,6 +11,9 @@ register = template.Library()
 
 @register.simple_tag
 def vote_exists(model, user=AnonymousUser(), action=UP):
-    if user.is_anonymous():
+    if get_version() >= '2.0':
+        if user.is_anonymous:
+            return False
+    elif user.is_anonymous():
         return False
     return model.votes.exists(user.pk, action=action)
