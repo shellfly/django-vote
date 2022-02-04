196b0975ea5d429bf73f494636261c676e086390
shellfly <shell0fly@gmail.com>
2022-02-04 17:23:58 +0800

    add secret key

--- runtests.py
+++ runtests.py
@@ -6,48 +6,47 @@ from django.core.management import execute_from_command_line
 
 if not settings.configured:
     settings.configure(
+        SECRET_KEY="secret key",
         DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
             }
         },
         INSTALLED_APPS=[
-            'django.contrib.auth',
-            'django.contrib.sessions',
-            'django.contrib.contenttypes',
-            'vote',
-            'test',
+            "django.contrib.auth",
+            "django.contrib.sessions",
+            "django.contrib.contenttypes",
+            "vote",
+            "test",
         ],
         ROOT_URLCONF="test.urls",
         MIDDLEWARE_CLASSES=[  # Deprecated in Django 1.10 https://docs.djangoproject.com/en/1.11/ref/settings/#middleware-classes
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
         ],
         MIDDLEWARE=[
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
         ],
         TEMPLATES=[
             {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'APP_DIRS': True,
-                'OPTIONS': {
-                    'context_processors': [
-                        'django.template.context_processors.request',
-                        'django.contrib.auth.context_processors.auth',
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "APP_DIRS": True,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
                     ],
-
                 },
             },
-
-        ]
+        ],
     )
 
 
 def runtests():
-    argv = sys.argv[:1] + ['test'] + sys.argv[1:]
+    argv = sys.argv[:1] + ["test"] + sys.argv[1:]
     execute_from_command_line(argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     runtests()
