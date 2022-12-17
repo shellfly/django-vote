b2e8eb4251ff09ede1974c94cbb7ebc3216b1c88
shellfly <shell0fly@gmail.com>
2022-12-17 17:54:49 +0800

    feat: add template tags

--- vote/templatetags/vote.py
+++ vote/templatetags/vote.py
@@ -3,13 +3,26 @@ from __future__ import absolute_import
 from django import template
 from django.contrib.auth.models import AnonymousUser
 
-from vote.models import UP
+from vote.models import UP, DOWN
 
 register = template.Library()
 
 
 @register.simple_tag
-def vote_exists(model, user=AnonymousUser(), action=UP):
+def vote_exists(model, user=AnonymousUser(), action="UP"):
     if user.is_anonymous:
         return False
+    if action.lower() == "up":
+        action = UP
+    else:
+        action = DOWN
     return model.votes.exists(user.pk, action=action)
+
+
+@register.simple_tag
+def vote_count(model, action="UP"):
+    if action.lower() == "up":
+        action = UP
+    else:
+        action = DOWN
+    return model.votes.count(action=action)
