eb52d7dd2ff1dd4638a070e7e59b84bec3b0aba2
Daniel Shafer <daniel@shafer.cc>
2024-06-20 10:02:40 -0600

    Fix migrations dependency

--- vote/migrations/005_update_field_type.py
+++ vote/migrations/005_update_field_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('vote', '0005_alter_vote_id'),
+        ('vote', '0004_auto_20170110_1150'),
     ]
 
     operations = [
